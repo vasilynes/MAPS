3a038cf46d97545a8e4fd591dc74e49789142578
vasilynes <ivyylas@gmail.com>
2026-06-14 20:27:09 +0700

    log-prob is wrapped in a class for TFP compatibility

--- src/t_hmm.py
+++ src/t_hmm.py
@@ -1,15 +1,65 @@
 import jax.numpy as jnp
 import jax.random as jr
 from jax import jit, grad
-import dynamax.hidden_markov_model as hmm
+import tensorflow_probability.substrates.jax.distributions as tfd
 
-def t_log_prob(x, loc, scale, df):
-    """Log-probability of the location-scale t-distribution."""
-    z = (x - loc) / scale
-    log_prob = (jnp.lgamma((df + 1) / 2) 
-                - jnp.lgamma(df / 2) 
-                - 0.5 * jnp.log(df * jnp.pi) 
-                - jnp.log(scale) 
-                - (df + 1) / 2 * jnp.log1p(z**2 / df))
-    return log_prob.sum()
+class StudentTDistribution(tfd.Distribution):
+    """
+    Multivariate location-scale Student distribution p(x | loc, scale, df).
+    The emission dims are assumed to be independent, univariate, t-distributed.
+    """
+    def __init__(
+            self,
+            loc, 
+            scale,
+            df, 
+            validate_args=False,
+            name='StudentTDistribution'
+    ):
+        self._loc = loc
+        self._scale = scale
+        self._df = df
+        super().__init__(
+            dtype=jnp.float32,
+            reparameterization_type=tfd.FULLY_REPARAMETERIZED,
+            validate_args=validate_args,
+            allow_nan_stats=True,
+            name=name
+        )
 
+    def _log_prob(self, x):
+        z = (x - self._loc) / self._scale
+        lp = (
+            jnp.lgamma((self._df + 1) / 2)
+            - jnp.lgamma(self._df / 2)
+            - 0.5 * jnp.log(self._df * jnp.pi)
+            - jnp.log(self._scale)
+            - (self._df + 1) / 2 * jnp.log1p(z ** 2 / self._df)
+        )
+        return lp.sum(axis=-1) # Sum over emission dimensions
+    
+    def _sample_n(self, n, seed):
+        """
+        Sample via Gaussian-chi2:
+        x = loc + scale * Z / sqrt(V/df)
+        """
+        key1, key2 = jr.split(seed)
+        shape = (n,) + self._loc.shape
+        z = jr.normal(key1, shape)
+        v = jr.gamma(key2, self._df / 2, shape) * 2 
+        return self._loc + self._scale * z / jnp.sqrt(v / self._df)
+    
+    def _event_shape(self):
+        return self._loc_shape
+    
+    def _event_shape_tensor(self):
+        return jnp.array(self._loc.shape, dtype=jnp.int32)
+    
+    def _batch_shape(self):
+        return ()
+    
+    def _batch_shape_tensor(self):
+        return jnp.zeros([], dtype=jnp.int32).shape
+    
+
+    
